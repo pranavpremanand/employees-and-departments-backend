98123b7b5102e955a724e8b856ca9b335cd0707d
Pranav Premanand <mpranavprem@gmail.com>
2023-03-23 16:01:55 +0530

    nodemon uninstalled

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ const port = 5000;
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", route);
-app.listen(port, () => console.log("Server is running on port:", port));
-
 mongoose.connect(
-    "mongodb://localhost:27017");
-const connection = mongoose.connection;
-connection
+  "mongodb://localhost:27017");
+  const connection = mongoose.connection;
+  connection
   .on("connected", () => console.log("Database is connected successfully!"))
   .on("error", (err) => console.log("Database connection failed", err));
-
+app.use("/", route);
+  app.listen(port, () => console.log("Server is running on port:", port));
+  
 module.exports = app;
